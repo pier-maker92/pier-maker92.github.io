09d08b6ac894fd275bde29852a1143388dd348b7
pier-maker92 <pierfrancesco.melucci@gmail.com>
2025-08-10 13:10:56 +0200

    fix index routing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pierfrancesco-melucci-portfolio',
+  base: process.env.NODE_ENV === 'production' ? '/pierfrancesco-melucci-portfolio' : '/',
   server: {
     port: 3000,
     open: true
